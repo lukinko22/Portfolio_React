a03c9394a85c53a2f72708eff680df9e7d2ebc08
Lukas <lukinko22@yahoo.co.uk>
2023-08-14 08:22:13 +0100

    added Skills.jsx

---
+++ src/components/Skills.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import Github from '../assets/github.png';
+import Tailwind from '../assets/tailwind.png';
+
+
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#101a28]  text-gray-300'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='text-center pb-8 pl-4'>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
+                <p className='py-4'>//These are the technologies I've worked with.</p>
+            </div>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-6 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 '>
+                    <img  className='w-20 mx-auto'src={HTML} alt='HTML ion'/>
+                    <p className='my-4'>HTML</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 '>
+                    <img  className='w-20 mx-auto'src={CSS} alt='HTML ion'/>
+                    <p className='my-4'>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img  className='w-20 mx-auto'src={JavaScript} alt='HTML ion'/>
+                    <p className='my-4'>JavaScript</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img  className='w-20 mx-auto'src={ReactImg} alt='HTML ion'/>
+                    <p  className='my-4'>React</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img  className='w-20 mx-auto'src={Github} alt='HTML ion'/>
+                    <p className='my-4'>Github</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img  className='w-20 mx-auto'src={Tailwind} alt='HTML ion'/>
+                    <p className='my-4'>Tailwind</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Skills
