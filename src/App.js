4c2dcf4b41c2a2f60671a3dc433420ef30513c84
Lukas <lukinko22@yahoo.co.uk>
2023-08-14 07:43:05 +0100

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div>
-    <h1 className="text-2xl">Hello</h1>
+    <Navbar />
+    <Home />
+    <About />
+    <Skills />
+    <Work />
+    <Contact />
     </div>
   );
 }
